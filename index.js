5f8d2db302ba90c56ca98562d8a6998afc70532f
zhangping168@gmail.com <zhangping168@gmail.com>
2016-07-15 13:50:24 -0400

    Update filter search functions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.get('/',function(req, res){
 	res.send('Todo-api root');
 });
-
+//GET /todos?completed=true&q=work
 app.get('/todos', function(req, res){
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -21,6 +21,12 @@ app.get('/todos', function(req, res){
 		filteredTodos = _.where(filteredTodos,{completed:true});
 	}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
 		filteredTodos = _.where(filteredTodos,{completed: false});
+	}
+	
+	if(queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0){
+		filteredTodos = _.filter(filteredTodos,function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.trim().toLowerCase()) > -1;
+		});
 	}
 	
 	res.json(filteredTodos);
