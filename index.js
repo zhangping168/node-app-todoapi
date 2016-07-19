e62b1523e5f4191c90e1d2110528cfe96e27d6a0
zhangping168@gmail.com <zhangping168@gmail.com>
2016-07-19 00:35:56 -0400

    Convert GET /todos/:id to use sequelize

--- index.js
+++ index.js
@@ -38,18 +38,18 @@ app.get('/todos', function (req, res) {
 	res.json(filteredTodos);
 })
 
+//GET todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos, {
-			id : todoId
-		});
-
-	if (matchedTodo) {
-
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send('ID not found');
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.send(todo.toJSON());
+		}else{
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send(); //server error;
+	});
 });
 
 //DELETE
@@ -70,12 +70,14 @@ app.delete ('/todos/:id', function (req, res) {
 //UPDATE
 app.put('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id);
+
+	/*
 	var matchedTodo = _.findWhere(todos, {
-			id : todoId
-		});
+	id : todoId
+	});
 
 	if (!matchedTodo) {
-		return res.status(404).send();
+	return res.status(404).send();
 	}
 
 	var body = _.pick(req.body, 'description', 'completed');
@@ -83,26 +85,27 @@ app.put('/todos/:id', function (req, res) {
 
 	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
 
-		validAttributes.completed = body.completed;
+	validAttributes.completed = body.completed;
 
 	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send(); //unable to complete
+	return res.status(400).send(); //unable to complete
 	} else {
-		//bad data,not sending anything
+	//bad data,not sending anything
 	}
 
 	if (body.hasOwnProperty('description') && _.isString(body.description)) {
 
-		validAttributes.description = body.description.trim();
+	validAttributes.description = body.description.trim();
 
 	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send(); //unable to complete
+	return res.status(400).send(); //unable to complete
 	} else {
-		//bad data,not sending anything
+	//bad data,not sending anything
 	}
 
 	_.extend(matchedTodo, validAttributes);
 	res.json(matchedTodo);
+	 */
 });
 
 //POST
@@ -115,21 +118,6 @@ app.post('/todos', function (req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
-	/*
-	//valid each field type
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	return res.status(400).send(); //unable to complete
-	}
-
-	//set body description to be trimmed value
-	body.description = body.description.trim();
-
-	body.id = todoNextId;
-	todos.push(body);
-	todoNextId++;
-
-	res.json(body);
-	 */
 
 });
 
