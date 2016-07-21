a6c96356637bf937d1403fa53884ae80a7c86926
zhangping168@gmail.com <zhangping168@gmail.com>
2016-07-21 01:43:33 -0400

    Add bcrypt hash to password

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post('/users', function(req, res){
 	var body = req.body;
 	body = _.pick(body, 'email', 'password');
 	db.user.create(body).then(function(user){
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	},function(e){
 		res.status(400).json(e);
 	});
--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt-nodejs');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email : {
@@ -8,19 +11,39 @@ module.exports = function (sequelize, DataTypes) {
 				isEmail : true
 			}
 		},
+		salt : {
+			type : DataTypes.STRING
+		},
+		password_hash : {
+			type : DataTypes.STRING
+		},
 		password : {
 			type : DataTypes.STRING,
 			allowNull : false,
 			validate : {
 				len : [7, 100]
+			},
+			set : function (value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
-	},{
-		hooks:{
-			beforeValidate:function(user, options){
-				if(typeof user.email === 'string'){
+	}, {
+		hooks : {
+			beforeValidate : function (user, options) {
+				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
+			}
+		},
+		instanceMethods :{
+			toPublicJSON : function () {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
